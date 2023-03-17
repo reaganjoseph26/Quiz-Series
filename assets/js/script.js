96578195434f50fd02a28675141f3cbb63e23199
Racer <reaganjoseph26@yahoo.com>
2023-03-16 21:46:59 -0400

    pictures and basic html layour added

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var movieThemeDiv = document.querySelector('#movieThemes')
+var showsThemeDiv = document.querySelector('#movieThemes')
+var musicThemeDiv = document.querySelector('#movieThemes')
+
+movieThemeDiv.addEventListener("click", function() {
+    console.log('movie click')
+})
+showsThemeDiv.addEventListener("shows click", function() {
+    console.log('click')
+})
+musicThemeDiv.addEventListener(" music click", function() {
+    console.log('click')
+})
